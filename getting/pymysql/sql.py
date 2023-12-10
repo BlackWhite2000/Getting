1139b5018a8fb6744336b76da0589bca53d20d3f
黑白 <2285697543@qq.com>
2023-12-10 17:04:06 +0800

    1.1.4

--- getting/pymysql/sql.py
+++ getting/pymysql/sql.py
@@ -9,4 +9,10 @@ def query(cursor, sql, params=None, is_execute=False):
     results = None
     if is_execute is False:
         results = cursor.fetchall()
+        if results:
+            # 获取字段名
+            column_names = [desc[0] for desc in cursor.description]
+            # 构建字典列表，每个字典表示一行数据
+            results = [dict(zip(column_names, row)) for row in results]
+
     return execute, results
