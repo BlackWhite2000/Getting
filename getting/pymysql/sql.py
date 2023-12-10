a788e996f70a59baea0166211a5f0d99ae337657
黑白 <2285697543@qq.com>
2023-12-10 16:32:04 +0800

    1.1.2

--- getting/pymysql/sql.py
+++ getting/pymysql/sql.py
@@ -2,10 +2,11 @@ def query(cursor, sql, is_execute=False):
     "pymysql 查询"
     # 执行查询
     execute = cursor.execute(sql)
-    if execute < 0:
+    if execute < 1:
         execute = None
     # 获取查询结果
     results = None
-    if is_execute is not None:
+    if is_execute is False:
+        print(is_execute)
         results = cursor.fetchall()
     return execute, results
