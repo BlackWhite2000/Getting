a5b228a82b60496da0c69eaedeafad70d11ac85e
BlackWhite <2285697543@qq.com>
2024-09-28 16:40:41 +0800

    1.3.8

--- getting/encrypt.py
+++ getting/encrypt.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 # AES加密
 def aes_encrypt(data, key, iv):
@@ -11,10 +12,13 @@ def aes_encrypt(data, key, iv):
 
     iv: 偏移量
     """
+    data = data.encode('utf-8')
     key = key.encode('utf-8') if isinstance(key, str) else key
     iv = iv.encode('utf-8') if isinstance(iv, str) else iv
-    aes = AES.new(key,AES.MODE_CBC,iv)
-    return aes.encrypt(data)
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    padded_data = pad(data, AES.block_size)
+    encrypted_data = aes.encrypt(padded_data)
+    return base64.b64encode(encrypted_data).decode('utf-8') 
 
 # AES解密
 def aes_decrypt(rData, key, iv):
@@ -42,12 +46,6 @@ import os
 import base64
 import json
 
-# # 当前脚本路径
-# path = os.path.dirname(os.path.abspath(__file__))
-# # 拼接公钥、私钥文件路径
-# pub_key_path = os.path.join(path, "pub.pem")
-# pri_key_path = os.path.join(path, "pri.pem")
-
 # 读取公钥
 def read_pub_key(path):
     """
@@ -67,32 +65,36 @@ def read_pri_key(path):
     return pri_key
 
 # RSA加密
-def rsa_encrypt(data,pub_key_path):
+def rsa_encrypt(data, pub_key_path):
     """
     RSA加密
 
     data: 待加密数据
     """
+    # 读取公钥
+    pub_key = read_pub_key(pub_key_path)
     # 使用公钥加密
-    encrypted_data = rsa.encrypt(data.encode(), read_pub_key(pub_key_path))
+    encrypted_data = rsa.encrypt(data.encode(), pub_key)
     # 将加密后的数据进行 Base64 编码以便传输
     return base64.b64encode(encrypted_data).decode()
 
 # RSA解密
-def rsa_decrypt(rData,pri_key_path):
+def rsa_decrypt(rData, pri_key_path):
     """
     RSA解密
 
     rData: 加密数据
     """
+    # 读取私钥
+    pri_key = read_pri_key(pri_key_path)
     decoded_data = base64.b64decode(rData)
-    decrypted_data = rsa.decrypt(decoded_data, read_pri_key(pri_key_path))
+    decrypted_data = rsa.decrypt(decoded_data, pri_key)
     return decrypted_data.decode()
 
-# 解密ARS加密数据再解密AES加密数据
-def decrypt_data(data,pri_key_path):
+# 解密ARS加密数据再解密RSA加密数据
+def decrypt_data(data, pri_key_path):
     """
-    解密 ARS 加密数据再解密 AES 加密数据
+    解密 ARS 加密数据再解密 RSA 加密数据
 
     data: RSA 加密数据
     """
@@ -101,7 +103,7 @@ def decrypt_data(data,pri_key_path):
     # 将 JSON 字符串解析为字典
     jsonData = json.loads(data)
     # 解密 RSA 加密数据，得到包含 AES 加密数据的 JSON 字符串
-    arsData = rsa_decrypt(jsonData.get('v'), rsa_decrypt(pri_key_path))
+    arsData = rsa_decrypt(jsonData.get('v'), pri_key_path)
     # 将 RSA 解密后的 JSON 字符串解析为字典
     arsData = json.loads(arsData)
     # 使用 AES 解密数据
@@ -110,13 +112,25 @@ def decrypt_data(data,pri_key_path):
 
 # 测试使用
 if __name__ == "__main__":
+    # 当前脚本路径
+    path = os.path.dirname(os.path.abspath(__file__))
+    # 拼接公钥、私钥文件路径
+    pub_key_path = os.path.join(path, "pub.pem")
+    pri_key_path = os.path.join(path, "pri.pem")
+    
     data = "hello"
     print("原始数据：", data)
     # AES加密
     key = "1234567890123456"
     iv = "1234567890123456"
     rData = aes_encrypt(data, key, iv)
-    print("AES加密后：", type(rData))
+    print("AES加密后：", rData)
     # AES解密
     dData = aes_decrypt(rData, key, iv)
     print("AES解密后：", dData)
+    # RSA加密
+    rData = rsa_encrypt(data, pub_key_path)
+    print("RSA加密后：", rData)
+    # RSA解密
+    dData = rsa_decrypt(rData, pri_key_path)
+    print("RSA解密后：", dData)
